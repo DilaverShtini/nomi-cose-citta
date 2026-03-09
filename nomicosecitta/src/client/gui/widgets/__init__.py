12e264e74f026148ddf7de89dc831f7f6e756cee
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2026-03-09 16:42:47 +0100

    refactor: extract VotingPanel widget from GameScreen

--- nomicosecitta/src/client/gui/widgets/__init__.py
+++ nomicosecitta/src/client/gui/widgets/__init__.py
@@ -1,5 +1,6 @@
 from src.client.gui.widgets.timer_display import TimerDisplay
-from src.client.gui.widgets.player_list import PlayerList
-from src.client.gui.widgets.chat_panel import ChatPanel
+from src.client.gui.widgets.player_list  import PlayerList
+from src.client.gui.widgets.chat_panel   import ChatPanel
+from src.client.gui.widgets.voting_panel import VotingPanel
 
-__all__ = ['TimerDisplay', 'PlayerList', 'ChatPanel']
+__all__ = ['TimerDisplay', 'PlayerList', 'ChatPanel', 'VotingPanel']
